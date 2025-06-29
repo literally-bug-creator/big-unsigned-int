e19a1a2d7589bb092d853f69a6b1c6d62260ddb6
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-29 23:13:40 +0300

    #5: Refactor

--- big_unsigned_int/source/mul.cpp
+++ big_unsigned_int/source/mul.cpp
@@ -3,22 +3,17 @@
 
 namespace big_uint {
 namespace {
+using MulChunk = __uint128_t;
 constexpr size_t SMALL_BYTE_LENGTH = 16384;
 constexpr size_t MEDIUM_BYTE_LENGTH = 131072;
 constexpr size_t CHUNK_BITS = sizeof(Chunk) * 8;
-using MulChunk = __uint128_t;
 
 BigUInt simpleMul(const BigUInt& multiplicand, const BigUInt& multiplier) {
-    if (isZero(multiplicand) || isZero(multiplier)) {
-        return BigUInt{};
-    }
-
     const std::vector<Chunk>& lhsLimbs = getLimbs(multiplicand);
     const std::vector<Chunk>& rhsLimbs = getLimbs(multiplier);
     size_t lhsSize = lhsLimbs.size();
     size_t rhsSize = rhsLimbs.size();
     std::vector<Chunk> limbs(lhsSize + rhsSize, 0);
-
     for (size_t i = 0; i < lhsSize; i++) {
         Chunk carry = 0;
         for (size_t j = 0; j < rhsSize; j++) {
@@ -34,18 +29,25 @@ BigUInt simpleMul(const BigUInt& multiplicand, const BigUInt& multiplier) {
     return BigUInt{limbs};
 }
 
-BigUInt toomMul(const BigUInt& multiplicand, const BigUInt& multiplier) {}
+BigUInt karatsubaMul(const BigUInt& multiplicand, const BigUInt& multiplier) {
+    return simpleMul(multiplicand, multiplier);
+}
 
-BigUInt nntMul(const BigUInt& multiplicand, const BigUInt& multiplier) {}
+BigUInt nntMul(const BigUInt& multiplicand, const BigUInt& multiplier) {
+    return simpleMul(multiplicand, multiplier);
+}
 
 }  // namespace
 BigUInt mul(const BigUInt& multiplicand, const BigUInt& multiplier) {
+    if (isZero(multiplicand) || isZero(multiplier)) {
+        return makeZero();
+    }
     size_t maxByteLength = std::max(getByteLength(multiplicand), getByteLength(multiplier));
     if (maxByteLength <= SMALL_BYTE_LENGTH) {
         return simpleMul(multiplicand, multiplier);
     }
     if (maxByteLength <= MEDIUM_BYTE_LENGTH) {
-        return toomMul(multiplicand, multiplier);
+        return karatsubaMul(multiplicand, multiplier);
     }
     return nntMul(multiplicand, multiplier);
 }
