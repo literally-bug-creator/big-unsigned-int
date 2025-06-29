dc02dfe70a8b77aed6ea08d8882384ff96046333
literally-bug-creator <literallybugcreator@gmail.com>
2025-06-29 19:12:06 +0300

    #3: Refactor `getters`, `getChunks` -> `getLimbs`

--- big_unsigned_int/source/getters.hpp
+++ big_unsigned_int/source/getters.hpp
@@ -7,5 +7,5 @@ size_t getLength(const BigUInt& number);
 
 Chunk getChunk(const BigUInt& number, size_t index);
 
-const std::vector<Chunk>& getChunks(const BigUInt& number);
+const std::vector<Chunk>& getLimbs(const BigUInt& number);
 }  // namespace big_uint
