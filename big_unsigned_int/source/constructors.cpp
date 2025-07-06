2ea373195700578263b6dc45e41438afd5acddb5
literally-bug-creator <literallybugcreator@gmail.com>
2025-07-06 21:53:26 +0300

    Add the base impl. of the `makeBigUInt` function

--- big_unsigned_int/source/constructors.cpp
+++ big_unsigned_int/source/constructors.cpp
@@ -1,7 +1,35 @@
+#include <vector>
+
 #include "big_uint.hpp"
 
 namespace big_uint {
+
+BigUInt makeBigUInt(const std::vector<Digit>& digits) {
+    if (digits.empty()) {
+        return makeZero();
+    }
+
+    std::vector<Chunk> limbs;
+    const size_t CHUNK_SIZE = MAX_VALUE_LENGTH;
+    size_t total = digits.size();
+
+    while (total > 0) {
+        size_t blockSize = (total >= CHUNK_SIZE) ? CHUNK_SIZE : total;
+        size_t start = total - blockSize;
+        Chunk value = 0;
+
+        for (size_t i = start; i < total; ++i) {
+            value = value * 10 + static_cast<Chunk>(digits[i]);
+        }
+
+        limbs.push_back(value);
+        total -= blockSize;
+    }
+    return BigUInt{limbs};
+}
+
 BigUInt makeZero() {
     return BigUInt({});
 }
+
 }  // namespace big_uint
