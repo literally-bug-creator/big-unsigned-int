f00340be491ae464a0dea24f9260010e650596cc
literally-bug-creator <literallybugcreator@gmail.com>
2025-07-07 15:13:14 +0300

    Add a another constants

--- big_unsigned_int/source/constants.hpp
+++ big_unsigned_int/source/constants.hpp
@@ -1,13 +1,21 @@
 #include <cstdint>
 #include <string>
 
+#include "big_uint.hpp"
+
 namespace big_uint {
 constexpr uint64_t MAX_VALUE = 9999999999999999999ULL;
 constexpr uint16_t MAX_VALUE_LENGTH = 19;
 
-constexpr int8_t ONE = 1;
+constexpr Chunk ZERO_CHUNK = 0;
+
 constexpr int8_t ZERO = 0;
+constexpr int8_t ONE = 1;
+constexpr int8_t TWO = 2;
+constexpr int16_t TEN = 10;
 
 constexpr std::string ONE_STR = "1";
 constexpr std::string ZERO_STR = "0";
+
+constexpr char ZERO_CHAR = '0';
 }  // namespace big_uint
