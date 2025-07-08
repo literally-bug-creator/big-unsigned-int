14fd9e419b51e679b929cfd341493e3845446aa0
literally-bug-creator <literallybugcreator@gmail.com>
2025-07-08 19:06:35 +0300

    #33: Reimplement the `toString` function

--- big_unsigned_int/source/to_string.cpp
+++ big_unsigned_int/source/to_string.cpp
@@ -1,19 +1,40 @@
+#include <cstdint>
 #include <string>
 
+#include <sys/types.h>
+
 #include "big_uint.hpp"
+#include "getters.hpp"
 
 namespace big_uint {
+namespace {
+std::string addLeadingZeros(const std::string& numberStr) {
+    size_t numZeros = MAX_VALUE_LENGTH > numberStr.size() ? MAX_VALUE_LENGTH - numberStr.size() : 0;
+    return std::string(numZeros, '0') + numberStr;
+}
+}  // namespace
+
 std::string toString(const BigUInt& number) {
-    if (number.limbs.empty()) {
-        return "0";
+    static constexpr std::string ZERO_STR = "0";
+    if (isZero(number)) {
+        return ZERO_STR;
     }
     std::string result;
     result.reserve(getSize(number));
-    result += std::to_string(number.limbs.back());
-    for (auto it = number.limbs.rbegin() + 1; it != number.limbs.rend(); ++it) {
-        std::string limbStr = std::to_string(*it);
-        size_t numZeros = MAX_VALUE_LENGTH > limbStr.size() ? MAX_VALUE_LENGTH - limbStr.size() : 0;
-        result += std::string(numZeros, '0') + limbStr;
+    std::vector<Chunk> limbs = getLimbs(number);
+    uint64_t carry = 0;
+    for (size_t index = 0; index < limbs.size() - 1; index++) {
+        __uint128_t curr = limbs[index] + carry;
+        carry = static_cast<uint64_t>(curr / MAX_DEGREE_OF_TEN);
+        auto remainder = static_cast<uint64_t>(curr % MAX_DEGREE_OF_TEN);
+        std::string limbStr = addLeadingZeros(std::to_string(remainder));
+        result.insert(0, limbStr);
+    }
+    uint64_t last = number.limbs.back() + carry;
+    carry = last / MAX_DEGREE_OF_TEN;
+    result = std::to_string(last % MAX_DEGREE_OF_TEN) + result;
+    if (carry > 0) {
+        result = std::to_string(carry) + result;
     }
     return result;
 }
