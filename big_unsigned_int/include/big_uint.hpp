a4c72eab17e7fcb96ee6ccedadbfa9cce953ef07
literally-bug-creator <literallybugcreator@gmail.com>
2025-07-09 16:11:26 +0300

    #35: Make the functions `noexcept`

--- big_unsigned_int/include/big_uint.hpp
+++ big_unsigned_int/include/big_uint.hpp
@@ -18,38 +18,38 @@ struct BigUInt {
     std::vector<Chunk> limbs;
 };
 
-BigUInt makeBigUInt(const std::vector<Digit>& digits);
+BigUInt makeBigUInt(const std::vector<Digit>& digits) noexcept;
 
-BigUInt makeZero();
+BigUInt makeZero() noexcept;
 
-BigUInt add(const BigUInt& augend, const BigUInt& addend);
+BigUInt add(const BigUInt& augend, const BigUInt& addend) noexcept;
 
-BigUInt add(const BigUInt& augend, const BigUInt& addend, size_t shift);
+BigUInt add(const BigUInt& augend, const BigUInt& addend, size_t shift) noexcept;
 
-BigUInt sub(const BigUInt& minuend, const BigUInt& subtrahend);
+BigUInt sub(const BigUInt& minuend, const BigUInt& subtrahend) noexcept;
 
-BigUInt sub(const BigUInt& minuend, const BigUInt& subtrahend, size_t shift);
+BigUInt sub(const BigUInt& minuend, const BigUInt& subtrahend, size_t shift) noexcept;
 
-BigUInt mul(const BigUInt& multiplicand, const BigUInt& multiplier);
+BigUInt mul(const BigUInt& multiplicand, const BigUInt& multiplier) noexcept;
 
-size_t getSize(const BigUInt& number);
+size_t getSize(const BigUInt& number) noexcept;
 
-string toString(const BigUInt& number);
+string toString(const BigUInt& number) noexcept;
 
-bool isZero(const BigUInt& number);
+bool isZero(const BigUInt& number) noexcept;
 
-bool isEqual(const BigUInt& left, const BigUInt& right);
+bool isEqual(const BigUInt& left, const BigUInt& right) noexcept;
 
-bool isGreater(const BigUInt& left, const BigUInt& right);
+bool isGreater(const BigUInt& left, const BigUInt& right) noexcept;
 
-bool isLower(const BigUInt& left, const BigUInt& right);
+bool isLower(const BigUInt& left, const BigUInt& right) noexcept;
 
-bool isGreaterOrEqual(const BigUInt& left, const BigUInt& right);
+bool isGreaterOrEqual(const BigUInt& left, const BigUInt& right) noexcept;
 
-bool isLowerOrEqual(const BigUInt& left, const BigUInt& right);
+bool isLowerOrEqual(const BigUInt& left, const BigUInt& right) noexcept;
 
-BigUInt trim(const BigUInt& number);
+BigUInt trim(const BigUInt& number) noexcept;
 
-BigUInt round(const BigUInt& number, size_t newSize);
+BigUInt round(const BigUInt& number, size_t newSize) noexcept;
 
 }  // namespace big_uint
